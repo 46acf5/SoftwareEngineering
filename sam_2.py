77f4b0a80edd0f086cf7a4cb8b18a5a713504aae
Olinka Molinka <132182527+46acf5@users.noreply.github.com>
2024-09-25 00:31:03 +0500

    Update sam_2.py

--- sam_2.py
+++ sam_2.py
@@ -1,12 +1,3 @@
 x = "hello world"
 for i in range (len(x)):
     print (x[i])
-
-one = int(input("Введите значение переменной: "))
-if 0 < one <= 3:
-    print("Переменная больше 0 и меньше 4")
-    if 3 < one <= 6:
-        print("Переменная больше 3 и меньше 7")
-elif 6 < one <= 10:
-    print("Переменная больше 6 и меньше 11")
-else: print ("Ошибка ввода")
