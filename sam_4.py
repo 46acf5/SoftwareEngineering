294982cbed8b27fbbfb3a2d6f640a608b7d1d467
Olinka Molinka <132182527+46acf5@users.noreply.github.com>
2024-10-02 01:10:22 +0500

    Update sam_4.py

---
+++ sam_4.py
@@ -0,0 +1,11 @@
+def calcargs(*args):
+    if args:
+        average = sum(args)/len(args)
+        return average
+    else:
+        return 0
+
+if __name__ == '__main__':
+    result = calcargs(11,22,33,44)
+    print(f"Среднее арифметическое: {result}")
+    
