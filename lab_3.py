d6fe7614012bf269b3128528c12b901576daaf53
Olinka Molinka <132182527+46acf5@users.noreply.github.com>
2024-10-09 17:30:34 +0500

    Update lab_3.py

--- lab_3.py
+++ lab_3.py
@@ -1,8 +1,4 @@
-def personal_info(name, age, company='unnamed'):
-    print(f"Имя: {name} Возраст: {age} Компания: {company}")
-
-tom = ("Григорий", 22)
-personal_info(*tom)
-
-bob = ("Георгий", 41, "Yandex")
-personal_info(*bob)
+input_string = 'HelloWorld'
+result = tuple(input_string)
+print(result)
+print(list(result))
