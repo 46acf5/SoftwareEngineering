8c2a9661305c13aca08e3218244b510d835f75ed
Olinka Molinka <132182527+46acf5@users.noreply.github.com>
2024-10-09 17:29:42 +0500

    Update lab_2.py

---
+++ lab_2.py
@@ -0,0 +1,15 @@
+from pprint import pprint
+
+my_dict={'first': 'so easy'}
+
+def dict_maker (**kwargs):
+    my_dict.update(**kwargs)
+
+dict_maker (a1 = 1, a2 = 20, a3 = 54, a4 = 13)
+dict_maker(name="Михаил", age = 31, weight = 70, eyes_color='blue')
+pprint(my_dict)
+
+input_string = 'HelloWorld'
+result = tuple(input_string)
+print(result)
+print(list(result))
