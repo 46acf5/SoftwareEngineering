4b909726ba4b43441b8c36285ebe7fe9322679a5
Olinka Molinka <132182527+46acf5@users.noreply.github.com>
2024-10-30 01:26:43 +0500

    Update lab_3.py

--- lab_3.py
+++ lab_3.py
@@ -1,3 +1,16 @@
+class Car:  #конструктор класса Car
+    def __init__(self, make, model): #функция инициализации машины с параметрами
+        # бренда и модели
+        self.make = make #инициализация бренда
+        self.model = model #инициализация модели
+
+    def drive(self):
+        print(f"Driving the {self.make} {self.model}")
+
+my_car = Car("Toyota", "Corolla") # создание записи экземпляра машины
+#бренда Toyota, модели Corolla
+my_car.drive()
+
 class ElectricCar (Car): # конструктор класса ElectricCar, наследующего класс Car
     def __init__(self, make, model, battery_capacity): # функция инициализации машины
         # с параметрами бренда, модели и мощности батареи
